bd99e917d7f49ce5fcbf9cba6a9d68a9c9c16dc3
Chathura Colombage <dcdewaka@gmail.com>
2014-05-11 21:06:07 +0100

    Intermediate work towards the solution

--- Random/May11.hs
+++ Random/May11.hs
@@ -1,8 +1,11 @@
 -- http://www.reddit.com/r/dailyprogrammer/comments/24ypno/572014_challenge_161_medium_appointing_workers/
 
+import Data.List
+import qualified Data.Map as M
+
 data Worker = Worker { name :: !String } deriving (Show, Eq)
 
-data Job = Job { job :: !String } deriving (Show, Eq)
+data Job = Job { job :: !String } deriving (Show, Eq, Ord)
 
 
 -- Alice Wiring,Insulation,Plumbing
@@ -37,8 +40,30 @@ workerSkills = [ (alice, [wiring, insulation, plumbing])
                , (david, [plumbing])
                , (erin, [insulation, decoration, finances])]
 
+workCandidates :: Ord b => [(t, [b])] -> M.Map b [t]
+workCandidates = foldl workCandidate M.empty
+  where
+    workCandidate m (w, jobs) = foldl (updateJobsMap w) m jobs
+    updateJobsMap w m j = M.alter (appendWorker w) j m
+    appendWorker w Nothing = Just [w]
+    appendWorker w (Just ws) = Just (w:ws)
+
+-- Prints out job and possible workers in the ascending order of availability
+-- This will give a good idea how to fill the buckets of job positions with
+-- all available candidates without leaving anyone out
+printJobsByAvailabilityOrder = do
+  let candidatesList = M.toList $ workCandidates workerSkills
+      candidateCount (_,as) (_,bs) = length as `compare` length bs
+      orderedList = sortBy candidateCount candidatesList
+  mapM_ print orderedList
+
 allocate :: [(Job, Worker)]
 allocate = undefined
+
+-- How to find the solution
+-- build a list of worker -> job list
+-- then fill job number of buckets with workers. sort this list based on versatality
+-- basis. that means david who can only do plumbing will be at the head
 
 main :: IO ()
 main = do

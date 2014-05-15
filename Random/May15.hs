07f4c2b777801ab38a8ff284466404b65fa903a3
Chathura Colombage <dcdewaka@gmail.com>
2014-05-15 23:49:49 +0100

    Points free practice

---
+++ Random/May15.hs
@@ -0,0 +1,28 @@
+-- This is some practice on point free form
+
+-- Count elemnts equal to a given element in a list
+countElem :: Eq a => a -> [a] -> Int
+countElem e ls = length $ filter (==e) ls
+
+countElem' :: Eq a => a -> [a] -> Int
+countElem' e = length . filter (==e)
+
+countElem'' :: Eq a => a -> [a] -> Int
+countElem'' = (length .) . filter . (==)
+
+-- Check if a list starts with a given element
+startsWith :: Eq a => a -> [a] -> Bool
+startsWith x ls = x == head ls
+
+startsWith' :: Eq a => a -> [a] -> Bool
+startsWith' x = (x ==) . head
+
+startsWith'' :: Eq a => a -> [a] -> Bool
+startsWith'' = (. head) . (==)
+
+-- Sum all elements not equal to a given element
+sumNotEqualTo :: (Eq a, Num a) => a -> [a] -> a
+sumNotEqualTo x ls = sum $ filter (/=x) ls
+
+sumNotEqualTo' :: (Eq a, Num a) => a -> [a] -> a
+sumNotEqualTo' = (sum .) . filter . (/=)

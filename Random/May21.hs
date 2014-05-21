1b090cb8689f8fdef4391603ffbd768fcf3e4341
Chathura Colombage <dcdewaka@gmail.com>
2014-05-22 00:20:05 +0100

    Implemented similarityIndex function

--- Random/May21.hs
+++ Random/May21.hs
@@ -1,13 +1,31 @@
 -- http://www.reddit.com/r/dailyprogrammer/comments/263dp1/5212014_challenge_163_intermediate_fallouts/
 
 import Data.Char
+import Data.List
 
 wordListFile = "enable1.txt"
+
+-- nLengthWords n = filter lengthN
+--   where
+--     lengthN = flip (length . (==n))
+
+nLengthWords = filter . (. length) . (==)
 
 readWords :: String -> IO [String]
 readWords file = do
   str <- readFile file
   return $ lines str
+
+similarityIndex :: Eq a => [a] -> [a] -> Int
+similarityIndex [] [] = 0
+similarityIndex (x:xs) (y:ys)
+  | x == y = 1 + similarityIndex xs ys
+  | otherwise = similarityIndex xs ys
+similarityIndex _ _ = error "Comparison lists should be same length"
+
+mostSimilar :: Eq a => [a] -> [[a]] -> [([a], Int)]
+mostSimilar w pool = sortBy (\(_, l1) (_, l2) -> l2 `compare` l1)
+                     $ zip pool $ map (similarityIndex w) pool
 
 main :: IO ()
 main = do

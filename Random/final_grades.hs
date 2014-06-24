b9f3c33426c9b301aee5bcf1f38e86dd344cf10b
Chathura Colombage <dcdewaka@gmail.com>
2014-06-24 23:28:33 +0100

    Example scores

--- Random/final_grades.hs
+++ Random/final_grades.hs
@@ -1,6 +1,6 @@
 -- http://www.reddit.com/r/dailyprogrammer/comments/28gq9b/6182014_challenge_167_intermediate_final_grades/
 
-newtype FullName = FullName (String, String) deriving (Show, Eq)
+type FullName = (String, String)
 
 data ScoreRecord = ScoreRecord { name :: FullName
                                , scores :: [Int]
@@ -13,3 +13,8 @@ computeAverge ScoreRecord { scores = sc } = round $ sum_scores / n
     n = fromIntegral $ length sc
 
 
+vetter = ScoreRecord { name = ("Valerie", "Vetter")
+                     , scores = [79, 81, 78, 83, 80] }
+
+richie = ScoreRecord { name = ("Richie", "Rich")
+                     , scores = [88, 90, 87, 91, 86] }

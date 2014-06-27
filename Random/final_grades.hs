b8d9f536ff333f4816985641919e6db590201dff
Chathura Colombage <dcdewaka@gmail.com>
2014-06-27 14:33:50 +0100

    Read ScoreRecord function

--- Random/final_grades.hs
+++ Random/final_grades.hs
@@ -1,8 +1,11 @@
 -- http://www.reddit.com/r/dailyprogrammer/comments/28gq9b/6182014_challenge_167_intermediate_final_grades/
 
-type FullName = (String, String)
+import Data.List (words)
 
-data ScoreRecord = ScoreRecord { name :: FullName
+data Name = Name { firstName :: String
+                         , lastName :: String } deriving (Show, Eq)
+
+data ScoreRecord = ScoreRecord { name :: Name
                                , scores :: [Int]
                                } deriving (Show, Eq)
 
@@ -49,10 +52,52 @@ computeGrade record = grade $ fromIntegral score
 
     score = computeAverge record
 
+-- readScoreRecord :: String -> Maybe ScoreRecord
+readScoreRecord str =
+  let ws = words str
+  in if length ws >= 4
+     then let (fname, lname) = (ws !! 0, ws !! 2)
+              marks = map read $ drop 3 ws
+          in Just $ ScoreRecord { name = Name fname lname, scores = marks }
+     else Nothing
 
 -- Test data
-vetter = ScoreRecord { name = ("Valerie", "Vetter")
+vetter = ScoreRecord { name = Name "Valerie" "Vetter"
                      , scores = [79, 81, 78, 83, 80] }
 
-richie = ScoreRecord { name = ("Richie", "Rich")
+richie = ScoreRecord { name = Name "Richie" "Rich"
                      , scores = [88, 90, 87, 91, 86] }
+
+-- Challenge input
+{-
+Jennifer ,  Adams   100 70  85  86  79
+Bubba , Bo Bob  50  55  60  53  30
+Matt ,  Brown   72  82  92  88  79
+Ned ,   Bundy   73  75  80  79  88
+Alfred ,    Butler  80  90  70  100 60
+Sarah , Cortez  90  72  61  70  80
+William ,   Fence   88  86  83  70  79
+Casper ,    Ghost   80  85  87  89  90
+Opie ,  Griffith    90  90  90  90  90
+Tony ,  Hawk    60  60  60  72  72
+Kirstin ,   Hill    100 90  92  94  95
+Hodor , Hodor   40  50  53  62  33
+Clark , Kent    89  90  88  92  91
+Tyrion ,    Lannister   93  97  100 91  95
+Ken ,   Larson  70  80  85  73  79
+Stannis ,   Mannis  60  70  75  77  78
+Bob ,   Martinez    79  88  92  82  72
+Jean Luc ,  Picard  90  89  95  70  65
+Harry , Potter  73  75  77  69  73
+Jaina , Proudmoore  90  92  100 95  94
+Richie ,    Rich    88  90  87  91  86
+John ,  Smith   90  80  70  60  50
+Jon ,   Snow    70  70  70  70  72
+Arya ,  Stark   91  92  90  93  90
+Edwin , Van Clef    40  50  55  57  33
+Valerie ,   Vetter  79  81  78  83  80
+Katelyn ,   Weekes  90  95  92  93  97
+Wil  , Wheaton  70  80  75  71  77
+Steve , Wozniak 88  89  87  86  85
+Derek , Zoolander   80  81  85  88  90
+-}

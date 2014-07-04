0d029c1f08fe1c6766d8ad5a972936b1445f1bd2
Chathura Colombage <dcdewaka@gmail.com>
2014-07-04 23:54:34 +0100

    Suggestions for sentences

--- Random/homerow_spell_check.hs
+++ Random/homerow_spell_check.hs
@@ -1,10 +1,12 @@
 -- http://www.reddit.com/r/dailyprogrammer/comments/29od55/722014_challenge_169_intermediate_homerow_spell/
 
+import System.IO
+
 firstRow = "qwertyuiop"
 secondRow = "asdfghjkl"
 thirdRow = "zxcvbnm"
 
-dictFile = "brit-a-z.txt"
+dictFile = "Random/enable1.txt"
 
 shiftFrom :: String -> Char -> Int -> Maybe Char
 shiftFrom row c n =
@@ -38,11 +40,39 @@ validWords dict words = [w | w <- words, hasWord dict w]
 
 readDictionary :: FilePath -> IO Dict
 readDictionary file = do
-  contents <- readFile file
+  handle <- openFile file ReadMode
+  hSetEncoding handle utf8_bom
+  contents <- hGetContents handle
   return $ map (filter (/= '\r')) $ lines contents
 
 testValidWords :: FilePath -> String -> IO ()
 testValidWords file word = do
   dict <- readDictionary file
   let ps = possibleShiftedWords word
-  print $ length dict
+  print $ filter (hasWord dict) ps
+
+type Suggestion = (String, [String])
+
+printSuggestions :: Suggestion -> IO ()
+printSuggestions (word, suggestions) = do
+  putStr $ word ++ " -> "
+  print suggestions
+
+getSuggestions :: Dict -> String -> Suggestion
+getSuggestions dict word = (word, suggestions)
+  where
+    suggestions = filter (hasWord dict) $ possibleShiftedWords word
+
+printCorrectedSentence :: String -> IO ()
+printCorrectedSentence str = do
+  dict <- readDictionary dictFile
+  mapM_ (printWordOrSuggestions dict) $ words str
+  where
+    printWordOrSuggestions dict w =
+      if hasWord dict w
+      then putStrLn w
+      else printSuggestions $ getSuggestions dict w
+
+testSentence1 = "the quick ntpem fox jumped over rgw lazy dog"
+testSentence2 = "gwkki we are hyptzgsi martians rt zubq in qrsvr"
+testSentence3 = "a oweaib who fprd not zfqzh challenges should mt ewlst to kze"

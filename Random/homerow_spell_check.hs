48ea8dd83be73c40d4b7bce4487086064e9f4ccd
Chathura Colombage <dcdewaka@gmail.com>
2014-07-04 22:16:41 +0100

    Shift word and dictionary functions

--- Random/homerow_spell_check.hs
+++ Random/homerow_spell_check.hs
@@ -4,6 +4,8 @@ firstRow = "qwertyuiop"
 secondRow = "asdfghjkl"
 thirdRow = "zxcvbnm"
 
+dictFile = "brit-a-z.txt"
+
 shiftFrom :: String -> Char -> Int -> Maybe Char
 shiftFrom row c n =
   case lookup c $ zip row [0..] of
@@ -20,3 +22,27 @@ shift c n =
 
 shiftRight = shift
 shiftLeft c n = shift c (-n)
+
+shiftWord word n = [shift c n | c <- word]
+shiftWordRight = shiftWord
+shiftWordLeft word n = shiftWord word (-n)
+
+possibleShiftedWords word = [shiftWord word n | n <- [-2..2]]
+
+type Dict = [String]
+
+hasWord :: Dict -> String -> Bool
+hasWord dict word = word `elem` dict
+
+validWords dict words = [w | w <- words, hasWord dict w]
+
+readDictionary :: FilePath -> IO Dict
+readDictionary file = do
+  contents <- readFile file
+  return $ map (filter (/= '\r')) $ lines contents
+
+testValidWords :: FilePath -> String -> IO ()
+testValidWords file word = do
+  dict <- readDictionary file
+  let ps = possibleShiftedWords word
+  print $ length dict

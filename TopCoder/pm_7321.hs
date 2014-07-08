29ad7a6a12de06db45965f9e3c64c85324636c2e
Chathura Colombage <dcdewaka@gmail.com>
2014-07-08 20:24:57 +0100

    First solution to topcoder: 7321

--- TopCoder/pm_7321.hs
+++ TopCoder/pm_7321.hs
@@ -29,33 +29,67 @@
 -- -	The return value will be less than or equal to 2147483647.
 
 import Data.List (foldl')
+import Data.Char (ord)
 
 data CStr = Str String
-          | NStr Int String deriving (Eq)
+          | NStr Int [CStr] deriving (Eq)
 
 instance Show CStr where
   show (Str s) = s
-  show (NStr n s) = concat $ take n $ repeat s
+  show (NStr n s) = concat $ take n $ repeat (show s)
 
 uncompress :: [CStr] -> String
-uncompress = foldl' (\a c -> a ++ show c) []
+-- uncompress = foldl' (\a c -> a ++ show c) []
+uncompress [] = []
+uncompress (Str s:xs) = s ++ uncompress xs
+uncompress ((NStr n sub):xs) =
+  let fs = foldl' (\a c -> a ++ uncompress [c]) [] sub
+  in (concat $ take n $ repeat fs) ++ uncompress xs
+
+uncompressedLength = length . uncompress
+
+splitUntil :: (a -> Bool) -> [a] -> ([a], [a])
+splitUntil _ [] = ([], [])
+splitUntil p y@(x:xs)
+  | p x = ([], y)
+  | otherwise = let (fs, rs) = splitUntil p xs
+                in (x:fs, rs)
 
 -- TODO: Implement parsing function to read a String like 10342(76) and return
 -- a parsed CStr list like: [CStr "1034", NStr 2 "76"]
 parseCompressed :: String -> [CStr]
-parseCompressed = undefined
+parseCompressed str = go str []
+  where
+    go [] acc = [Str acc]
+    go (x:'(':xs) acc = let (fs, ps) = extractFirstParenthesis ('(':xs)
+                            n = ord x - ord '0'
+                        in [Str acc, NStr n $ parseCompressed fs] ++ parseCompressed ps
+    go (x:xs) acc = go xs (acc++[x])
 
-extractParenthesis :: String -> String
-extractParenthesis str = go str []
+extractFirstParenthesis :: String -> (String, String)
+extractFirstParenthesis str = go str []
   where
     go ('(':xs) [] = go xs ['(']
-    go ('(':xs) ps = '(' : go xs ('(':ps)
+    go ('(':xs) ps = let (ys,rs) = go xs ('(':ps)
+                     in  ('(':ys, rs)
     go (')':xs) [] = error "Unmatched parenthesis"
-    go (')':xs) ['('] = go xs []
-    go (')':xs) (_:ps) = ')' : go xs ps
-    go (x:xs) ps = x : go xs ps
-    go [] [] = []
+    go (')':xs) ['('] = ([], xs) -- Only one closing brace remaining. So this is the end of the first match. Return the string and the rest of non-consumed string
+    go (')':xs) (_:ps) = let (ys, rs) = go xs ps
+                         in (')':ys, rs)
+    go (x:xs) ps = let (ys, rs) = go xs ps
+                   in (x:ys, rs)
+    go [] [] = ([], [])
+
+testStrings = [ ("123", 3)
+              , ("10342(76)", 8)
+              , ("33(562(71(9)))", 19)
+              , ("0(0)", 0)
+              , ("1(1(1(1(1(1(1(0(1234567890))))))))", 0)
+              ]
+
+answer = mapM_ print $ map (uncompressedLength . parseCompressed . fst) testStrings
 
 main :: IO ()
 main = do
   putStrLn "*** CompressedString Problem ***"
+  answer
